46b3daa398a33f12c44277c258d0a8bb3e3d15dc
Jan Prachař <jan.prachar@gmail.com>
2014-02-03 14:05:49 +0100

    bugfix: correct url path of redirect location

--- crawler.c
+++ crawler.c
@@ -179,6 +179,7 @@ static int simpleparseurl(struct surl *u, const char *url) {
 	u->port = 0;
 	u->proto[0] = 0;
 	u->path[0] = '/';
+	u->path[1] = 0;
 
 	r = sscanf(url, "%31[^:]://%99[^:]:%99d%99[^\n]", u->proto, u->host, &(u->port), u->path);
 	if (r < 2) {
